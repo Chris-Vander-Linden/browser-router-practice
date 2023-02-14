a226aeee4b4bfc42d5cae32f7172052416671432
Chris Vander Linden <cvl123abc@gmail.com>
2023-02-14 13:23:01 -0600

    Browser Router is working

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class About extends React.Component {
+
+  render() {
+    return (
+      <div>This is the about us section.  Kind of fun.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione vero ea iusto impedit aperiam hic, cum earum distinctio velit, libero pariatur optio necessitatibus fuga, veritatis quas? Esse consectetur qui veniam?</div>
+    )
+  }
+}
+
+export default About;
---
+++ src/components/Books.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Books extends React.Component {
+
+  render() {
+    return (
+    <>
+      <div>book 1</div>
+      <div>book 2</div>
+      <div>book 3</div>
+    </>
+    )
+  }
+}
+
+export default Books;
